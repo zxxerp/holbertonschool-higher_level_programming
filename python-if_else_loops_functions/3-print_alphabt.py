9c620eeaa207a77aac3793a5f1a459c548daab21
zxxerp <ayxanhuseynzade12@gmail.com>
2025-11-15 19:08:47 +0000

    a

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    print("{}".format(chr(i)), end="")
-    if i = 'q' or i ='e':
-        continue
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
