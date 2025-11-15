7013b96215c0e39c9122be7badc8c64dac7d3a75
zxxerp <ayxanhuseynzade12@gmail.com>
2025-11-15 14:39:54 +0000

    a

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Script that prints "Welcome to Holberton School!" using two variables
+# print statement using both str1 and str2
 str1 = "Holberton"
 str2 = "School"
 print(f"Welcome to {str1} {str2}!")
