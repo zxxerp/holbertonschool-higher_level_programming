133c5b119aeec03ccb80bd7bcb472d2b21bac754
zxxerp <ayxanhuseynzade12@gmail.vom>
2025-11-19 10:45:02 +0000

    a

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
             if i != len(row) - 1:
                 print("{:d}".format(row[i]), end= " ")
             else:
-                print("{:d}".format(row[i]), end= "")
+                print("{:d}".format(row[i]), end="")
         print()
