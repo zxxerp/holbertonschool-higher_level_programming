89b7c5fbd505c6ff4a959b84d8947f85bc340d2f
zxxerp <ayxanhuseynzade12@gmail.com>
2025-11-22 18:19:16 +0000

    a

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not in my_list:
+    """
+    Finds the biggest integer of a list without using max().
+    """
+    # 1st. Edge Case Check: If the list is empty, return None
+    if not my_list:
         return None
-    
-    b = sort(my_list)
-    i = b[-1]
-    return (i)
+
+    # 2nd. Set the initial record (biggest) to the first element
+    biggest = my_list[0]
+
+    # 3rd. Loops through every number in the list
+    for number in my_list:
+        # 4th. Comparison: If the current number breaks the record, update it
+        if number > biggest:
+            biggest = number
+
+    # 5th. After the loop, returns the final biggest number found
+    return biggest
