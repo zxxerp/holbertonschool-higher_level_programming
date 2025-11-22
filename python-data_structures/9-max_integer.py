eca07de8f12c17b5d9020503e02482b5715a5796
zxxerp <ayxanhuseynzade12@gmail.com>
2025-11-22 17:01:40 +0000

    a

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my list)==0:
-        i = None
+    if not in my_list:
+        return None
     else:
         i = sort(my_list)
         i = i[-1]
