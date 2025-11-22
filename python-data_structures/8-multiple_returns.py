54132831bc92b095b4aeecb3798a7a2d6ea72a4b
zxxerp <ayxanhuseynzade12@gmail.com>
2025-11-22 16:27:03 +0000

    a

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-
+    """
+    Returns a tuple with the length of a string
+    and its first character.
+    """
+    # 1st. Calculates the length of the sentence
     length = len(sentence)
 
-   
+    # 2nd. Checks if the sentence is empty
     if length == 0:
-   
+        # If empty, the first character must be None
         first_char = None
     else:
-   
+        # If not empty, we can safely grab the first character
         first_char = sentence[0]
 
-   
-   return (length, first_char)
+    # 3rd. Returns both values packed in a tuple
+    return (length, first_char)
