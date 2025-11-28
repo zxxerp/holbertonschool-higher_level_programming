b968a531fe63018199f014f8a67e395a35fa16bb
zxxerp <ayxanhuseynzade12@gmail.com>
2025-11-28 08:33:41 +0000

    a

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module 4-inherits_from:
+Defines a function to check if an object's class inherits from a specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """Return True if obj is an instance of a class that inherited
+    (directly or indirectly) from a_class, otherwise False.
+
+    Example:
+    >>> inherits_from(True, int)
+    True
+    >>> inherits_from(True, bool)
+    False
+    >>> inherits_from([], object)
+    True
+    >>> inherits_from(3, object)
+    True
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
