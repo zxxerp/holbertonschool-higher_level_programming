bce203b312d3e979cf4de897f9b851a916ba3951
zxxerp <ayxanhuseynzade12@gmail.com>
2025-11-30 13:09:32 +0000

    a

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- second_table in the database 
+CREATE TABLE second_table(
+	id BIGINT NOT NULL  PRIMARY KEY
+	name VARCHAR(256)
+	score NOT NULL  BIGINT
+);
+INSERT INTO second_table( id, name, score)
+VALUES (1, "JOHN", 10), (2, "ALEX", 3), (3, 'Bob', 14),  (4, 'George', 8);
+
