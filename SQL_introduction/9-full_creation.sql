0478efec70bddd9c8885a763bbd29a3eac1c7b70
zxxerp <ayxanhuseynzade12@gmail.com>
2025-11-30 13:12:23 +0000

    a

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS second_table(
 	name VARCHAR(256)
 	score NOT NULL  BIGINT
 );
+-- inserts the specified records
 INSERT INTO second_table( id, name, score)
 VALUES (1, "JOHN", 10), (2, "ALEX", 3), (3, 'Bob', 14),  (4, 'George', 8);
 
